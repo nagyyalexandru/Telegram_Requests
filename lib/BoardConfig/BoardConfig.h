7835ecf5cc69b034eb94d572ca15774e324701dc
Alexandru Nagy <nagyy.alexandru@gmail.com>
2025-04-11 15:31:10 +0300

    moved PINs mapping to config module

--- lib/BoardConfig/BoardConfig.h
+++ lib/BoardConfig/BoardConfig.h
@@ -1,6 +1,9 @@
 #ifndef BoardConfig_h
 #define BoardConfig_h
 
+#define CONFIG_TEMPERATURE_DHT_PIN 5
+#define CONFIG_TEMPERATURE_DHT_TYPE DHT11
+
 #define DEBUG_FLAG_ESP32_TELEGRAM_REQUEST
 
 void BoardConfig_Setup();
--- lib/TemperatureSensor/TemperatureSensor.h
+++ lib/TemperatureSensor/TemperatureSensor.h
@@ -5,10 +5,7 @@
 #ifndef TemperatureSensor_h
 #define TemperatureSensor_h
 
-#define TEMPERATURE_DHT_PIN 5
-#define TEMPERATURE_DHT_TYPE DHT11
-
-static DHT Temperature_DHT_Sensor(TEMPERATURE_DHT_PIN, TEMPERATURE_DHT_TYPE);
+static DHT Temperature_DHT_Sensor(CONFIG_TEMPERATURE_DHT_PIN, CONFIG_TEMPERATURE_DHT_TYPE);
 
 void TemperatureSensor_Setup();
 void TemperatureSensor_Loop();
